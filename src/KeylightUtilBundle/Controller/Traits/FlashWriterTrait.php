a1cb50118714f521c635256a710610e212408bc3
Daniel Werner <mail@danwer.de>
2016-09-28 11:46:20 +0200

    Automatically translating flash messages

--- src/KeylightUtilBundle/Controller/Traits/FlashWriterTrait.php
+++ src/KeylightUtilBundle/Controller/Traits/FlashWriterTrait.php
@@ -5,23 +5,49 @@ trait FlashWriterTrait
 {
     /**
      * @param string $message
+     * @param array $translationParameters
+     * @param string $domain
      */
-    protected function addNotice($message)
+    protected function addNotice($message, $translationParameters = [], $domain = null)
     {
-        $this->addFlash(
+        $this->addTranslatedFlash(
             'notice',
-            $message
+            $message,
+            $translationParameters,
+            $domain
         );
     }
 
     /**
      * @param string $message
+     * @param array $translationParameters
+     * @param string $domain
      */
-    protected function addError($message)
+    protected function addError($message, $translationParameters = [], $domain = null)
+    {
+        $this->addTranslatedFlash(
+            'error',
+            $message,
+            $translationParameters,
+            $domain
+        );
+    }
+
+    /**
+     * @param string $key
+     * @param string $message
+     * @param array $translationParameters
+     * @param string $domain
+     */
+    private function addTranslatedFlash($key, $message, $translationParameters = [], $domain = null)
     {
         $this->addFlash(
-            'error',
-            $message
+            $key,
+            $this->get('translator')->trans(
+                $message,
+                $translationParameters,
+                $domain
+            )
         );
     }
 }
