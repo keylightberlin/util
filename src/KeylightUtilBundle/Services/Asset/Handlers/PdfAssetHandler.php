c7be3e94a218f81d72686ecc4d386671a8ac628d
Iurii Kozhan <iurii.kozhan@gmail.com>
2016-11-17 14:50:25 +0100

    Rendering html for uploaded pdf

--- src/KeylightUtilBundle/Services/Asset/Handlers/PdfAssetHandler.php
+++ src/KeylightUtilBundle/Services/Asset/Handlers/PdfAssetHandler.php
@@ -18,8 +18,6 @@ class PdfAssetHandler implements AssetHandlerInterface
      */
     private $assetFactory;
 
-
-
     public function __construct(
         AssetStorageInterface $assetStorage,
         AssetFactoryInterface $assetFactory
@@ -33,28 +31,8 @@ class PdfAssetHandler implements AssetHandlerInterface
      */
     public function handleSave(Asset $asset)
     {
-        $newFilename = pathinfo($asset->getFilename(), PATHINFO_FILENAME) . '.png';
-
-        $documentImage = new \Imagick();
-        $documentImage->setResolution(300, 300);
-        $documentImage->readImage($asset->getUploadedFile()->getRealPath());
-        $documentImage->resetIterator();
-
-        $documentImage = $documentImage->appendImages(true);
-        $documentImage = $documentImage->flattenImages();
-        $documentImage->setImageFormat('png');
-
-        $childAsset = $this->assetFactory->getInstance();
-        $childAsset->setStorageType($asset->getStorageType());
-        $childAsset->setType($asset->getType());
-        $childAsset->setFilename($newFilename);
-
-        $childAsset->setHeight($documentImage->getImageHeight());
-        $childAsset->setWidth($documentImage->getImageWidth());
-        $childAsset->setFileContents($documentImage);
-
-        $asset->addChildAsset($childAsset);
-        $this->assetStorage->saveAsset($childAsset);
+        $this->generatePngForPdf($asset, 300);
+        $this->generateHtmlForPdf($asset);
     }
 
     /**
@@ -72,5 +50,63 @@ class PdfAssetHandler implements AssetHandlerInterface
     public function supportsAsset(Asset $asset)
     {
         return $asset->isPdf();
+    }
+
+    /**
+     * @param Asset $asset
+     * @param int $resolution
+     */
+    private function generatePngForPdf(Asset $asset, $resolution = 300)
+    {
+        $newFilename = pathinfo($asset->getFilename(), PATHINFO_FILENAME) . '.png';
+
+        $documentImage = new \Imagick();
+        $documentImage->setResolution($resolution, $resolution);
+        $documentImage->readImage($asset->getUploadedFile()->getRealPath());
+        $documentImage->resetIterator();
+        $documentImage = $documentImage->appendImages(true);
+        $documentImage = $documentImage->flattenImages();
+        $documentImage->setImageFormat('png');
+
+        $childAsset = $this->assetFactory->getInstance();
+        $childAsset->setStorageType($asset->getStorageType());
+        $childAsset->setType($asset->getType());
+        $childAsset->setFilename($newFilename);
+        $childAsset->setHeight($documentImage->getImageHeight());
+        $childAsset->setWidth($documentImage->getImageWidth());
+        $childAsset->setFileContents($documentImage);
+
+        $asset->addChildAsset($childAsset);
+        $this->assetStorage->saveAsset($childAsset);
+    }
+
+    /**
+     * @param Asset $asset
+     * @throws \Exception
+     */
+    private function generateHtmlForPdf(Asset $asset)
+    {
+        if (`which pdf2htmlEX`) {
+            $newFilename = pathinfo($asset->getFilename(), PATHINFO_FILENAME) . '.html';
+
+            $tempHtmlFile = tempnam(sys_get_temp_dir(), 'pdf2html');
+            exec("pdf2htmlEX --dest-dir " . dirname($tempHtmlFile) . ' ' . $asset->getUploadedFile()->getRealPath() . ' ' . basename($tempHtmlFile));
+
+            $handle = fopen($tempHtmlFile, "r");
+            $htmlFileContent = fread($handle, filesize($tempHtmlFile));
+            fclose($handle);
+            unlink($tempHtmlFile);
+
+            $childAsset = $this->assetFactory->getInstance();
+            $childAsset->setStorageType($asset->getStorageType());
+            $childAsset->setType($asset->getType());
+            $childAsset->setFilename($newFilename);
+            $childAsset->setFileContents($htmlFileContent);
+
+            $asset->addChildAsset($childAsset);
+            $this->assetStorage->saveAsset($childAsset);
+        } else {
+            throw new \Exception('pdf2htmlEX is not install');
+        }
     }
 }
