9c6ebc5fe5ec4c4fd493e5d39369220ed1d208f6
Jens Wübbenhorst <j.wuebbenhorst@keylight.de>
2018-01-17 12:55:12 +0100

    let health check return an error http code

--- src/KeylightUtilBundle/Controller/HealthCheckController.php
+++ src/KeylightUtilBundle/Controller/HealthCheckController.php
@@ -1,8 +1,10 @@
 <?php
 namespace KeylightUtilBundle\Controller;
 
+use KeylightUtilBundle\Model\Health\HealthCheck;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -17,8 +19,11 @@ class HealthCheckController extends Controller
      */
     public function healthCheckAction()
     {
+        /** @var HealthCheck $results */
+        $results = $this->get('keylight_util_health_check_provider')->getHealthCheck();
         return new JsonResponse(
-            $this->get('keylight_util_health_check_provider')->getHealthCheckAsArray()
+            $results->toArray(),
+            ($results->hasFailed()) ? Response::HTTP_SERVICE_UNAVAILABLE : Response::HTTP_OK
         );
     }
 }
